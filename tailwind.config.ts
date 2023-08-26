b107d2d6074ec17a29bf190a1fa6130ce34e6a7e
ASolisEk <angel.abaccor@gmail.com>
2023-08-26 00:16:09 +0000

    se agrego el normalize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		extend: {
+			spacing: {
+				'128': '32rem',
+				'144': '36rem',
+			},
+			borderRadius: {
+				'4xl': '2rem',
+			},
+			colors: {
+				primary: "#0f172a"
+			},
+			fontFamily: {
+				sans: ['Graphik', 'sans-serif'],
+				serif: ['Merriweather', 'serif'],
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+				'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+	plugins: [],
 }
 export default config
